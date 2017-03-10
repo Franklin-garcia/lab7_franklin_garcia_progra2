64c3f6df81fcef06216fc1e0552f9e815dbf5f4b
franklin garcia <franklin__garcia@unitec.edu>
2017-03-10 15:49:09 -0600

    haciendo tabla

--- lab7_franklin_garcia/src/lab7_franklin_garcia/hilo_batalla.java
+++ lab7_franklin_garcia/src/lab7_franklin_garcia/hilo_batalla.java
@@ -9,6 +9,21 @@ package lab7_franklin_garcia;
  *
  * @author Franklin Garcia
  */
-public class hilo_batalla {
+public class hilo_batalla implements Runnable {
+    private String id;
+    private guerreros jugador1;
+    private guerreros jugador2;
+
+    public hilo_batalla(String id, guerreros jugador1, guerreros jugador2) {
+        this.id = id;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+    }
+
+    @Override
+    public void run() {
+        
+    }
+    
     
 }
