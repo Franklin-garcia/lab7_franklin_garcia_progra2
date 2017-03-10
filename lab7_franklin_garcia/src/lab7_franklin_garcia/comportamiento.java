ba1c97a7cdfb38b233c604604db799b9d573dd00
franklin garcia <franklin__garcia@unitec.edu>
2017-03-10 13:58:59 -0600

    polimorfismo

---
+++ lab7_franklin_garcia/src/lab7_franklin_garcia/comportamiento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_franklin_garcia;
+
+/**
+ *
+ * @author Franklin Garcia
+ */
+public interface comportamiento {
+    public void ataque();
+}
